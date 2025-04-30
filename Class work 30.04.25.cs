4a78f2d7432760e3652ac55b3b1646080f2a3b48
MishaPetrovskui <154260100+MishaPetrovskui@users.noreply.github.com>
2025-04-30 20:47:31 +0300

    Create Class work 30.04.25.cs

---
+++ Class work 30.04.25.cs
@@ -0,0 +1,196 @@
+
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Http;
+using System.Text.Json;
+using System.IO;
+
+class Message
+{
+    public string user { get; set; }
+    public string text { get; set; }
+}
+
+class User
+{
+    public string name { get; set; }
+    public IPEndPoint ip { get; set; }
+}
+
+class Server
+{
+    static Random rnd = new Random();
+    static int port = 5056;
+    static List<User> ipEndPoints = new List<User>();
+
+    static bool IsNew(IPEndPoint endpoint)
+    {
+        foreach (var item in ipEndPoints)
+        {
+            if (item.ip.Equals(endpoint))
+                return true;
+        }
+        return false;
+    }
+
+    static void Broadcast(string message, UdpClient server, string name)
+    {
+        Console.WriteLine($"{name}: {message}");
+        byte[] data = Encoding.UTF8.GetBytes(name + " - " + message);
+        foreach (var item in ipEndPoints)
+        {
+            server.Send(data, item.ip);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        UdpClient server = new UdpClient(port);
+        /*
+                while (true)
+                {
+                    IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+                    byte[] data = server.Receive(ref remoteEP);
+
+                    string message = Encoding.UTF8.GetString(data);
+                    Console.WriteLine($"{remoteEP.Address.Address}: {message}");
+                    if (message.ToLower() == "ping")
+                    {
+                        server.Send(Encoding.UTF8.GetBytes("Pong"), remoteEP);
+                        Console.WriteLine("Send message: Pong");
+                    }
+                    else
+                    {
+                        server.Send(Encoding.UTF8.GetBytes("Not ping"), remoteEP);
+                        Console.WriteLine("Send message: Not ping");
+                    }
+                }*/
+        while (true)
+        {
+
+            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+            byte[] data = server.Receive(ref remoteEP);
+            string name = Encoding.UTF8.GetString(data);
+            User user = new User { name = name, ip = remoteEP };
+            if (IsNew(user.ip))
+            {
+                ipEndPoints.Add(user);
+            }
+            Console.WriteLine($"{user.name}: Added");
+            try
+            {
+                while (true)
+                {
+                    byte[] data1 = server.Receive(ref remoteEP);
+                    string text = Encoding.UTF8.GetString(data);
+                    if (text != null)
+                    {
+                        Broadcast(text, server, name);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
+
+
+// CLIENT 
+
+
+
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Text.Json;
+using System.Drawing;
+using System.Diagnostics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Message
+{
+    public string user { get; set; }
+    public string text { get; set; }
+}
+
+
+
+class Client
+{
+    static string serverIP = "127.0.0.1";
+    static int port = 5056;
+    static UdpClient client;
+    static IPEndPoint serverEP;
+    static void ReadingFromServer()
+    {
+        while (true)
+        {
+            try
+            {
+                string text = Encoding.UTF8.GetString(client.Receive(ref serverEP));
+
+                Console.WriteLine($"{text}");
+            }
+            catch (Exception ex) { break; }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = UTF8Encoding.UTF8;
+        Console.InputEncoding = UTF8Encoding.UTF8;
+        client = new UdpClient();
+
+        serverEP = new IPEndPoint(IPAddress.Parse(serverIP), port);
+        
+        Console.Write("Name: ");
+        string message = Console.ReadLine();
+        byte[] data = Encoding.UTF8.GetBytes(message);
+        /*
+                byte[] data;
+                Stopwatch a = new Stopwatch();
+                if (message.ToLower() == "ping")
+                {
+                    a = Stopwatch.StartNew();
+                    data = Encoding.UTF8.GetBytes(message);
+                }
+                else
+                {
+                    data = Encoding.UTF8.GetBytes(message);
+                }
+                client.Send(data, serverEP);
+                Console.WriteLine("Message send");
+
+                byte[] responce = client.Receive(ref serverEP);
+                string answer = Encoding.UTF8.GetString(responce);
+
+
+                if (answer == "Pong")
+                {
+                    a.Stop();
+                    Console.WriteLine(a.Elapsed);
+                    Console.WriteLine($"Catched message: {answer}");
+                }
+                else
+                    Console.WriteLine($"Catched message: {answer}");*/
+        Thread thread = new Thread (ReadingFromServer);
+        thread.Start();
+        while (true)
+        {
+            Console.Write("");
+            string text = Console.ReadLine();
+            if (text != null )
+            {
+                byte[] data1 = Encoding.UTF8.GetBytes(text);
+                client.Send(data1, serverEP);
+            }
+        }
+    }
+}
