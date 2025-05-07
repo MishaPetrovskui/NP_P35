758dce9cbca9ed7fca1fdd6dd7e67128353e9b0e
MishaPetrovskui <154260100+MishaPetrovskui@users.noreply.github.com>
2025-05-07 20:07:55 +0300

    Update Class work 07.05.25.cs

--- Class work 07.05.25.cs
+++ Class work 07.05.25.cs
@@ -1,3 +1,6 @@
+// server 
+
+
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
@@ -75,6 +78,146 @@ class UDPServerApp
             else { players.Add(remoteEP, player); }
             Player[] response = GetResponseForClient(remoteEP);
             server.Send(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(response)), remoteEP);
+        }
+    }
+}
+
+
+// client 
+
+
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Text.Json;
+using System.Drawing;
+using System.Diagnostics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using static System.Net.Mime.MediaTypeNames;
+
+class Player
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public Player(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+    public Player() : this(0, 0) { }
+}
+
+class Location
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int width { get; set; }
+    public int height { get; set; }
+    public List<Player> Players { get; set; } = new List<Player>();
+    public ConsoleColor odno_slovo { get; set; }
+    public ConsoleColor dwa_slova { get; set; }
+
+    public Location()
+    {
+        (X, Y) = (0, 0);
+        (width, height) = (20, 20);
+        odno_slovo = ConsoleColor.Green;
+        dwa_slova = ConsoleColor.Black;
+    }
+    public void Draw()
+    {
+        for (int y = 0; y < height; y++)
+        {
+            Console.SetCursorPosition(X, Y + y);
+            Console.BackgroundColor = odno_slovo;
+            Console.ForegroundColor = dwa_slova;
+            for (int x = 0; x < width; x++)
+            {
+                Player? player = GetPlayerByPosition(x, y);
+                if (player == null) { Console.Write("  "); }
+                else { Console.Write("[]"); }
+            }
+            Console.ResetColor();
+        }
+    }
+
+    public Player? GetPlayerByPosition(int x, int y)
+    {
+        Player[] players;
+        lock (Players)
+        {
+            players = Players.ToArray();
+        }
+        foreach (Player player in players)
+        {
+            if (player.X == x && player.Y == y)
+                return player;
+        }
+        return null;
+    }
+}
+
+class UDPClientApp
+{
+    static string serverIP = "127.0.0.1";
+    static int port = 5056;
+    static UdpClient client = new UdpClient();
+    static IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse(serverIP), port);
+    static Player player = new Player();
+    static Location location = new Location();
+    static readonly object lockObj = new object();
+    static Player[] _;
+
+    static void UpdatePlayer()
+    {
+        while (true)
+        {
+            client.Send(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(player)), serverEP);
+            string response = Encoding.UTF8.GetString(client.Receive(ref serverEP));
+            Player[] otherPlayers = JsonSerializer.Deserialize<Player[]>(response);
+            if (otherPlayers != null)
+            {
+                lock (lockObj)
+                {
+                    location.Players.Clear();
+                    location.Players.Add(player);
+                    location.Players.AddRange(otherPlayers);
+                }
+            }
+            Thread.Sleep(10);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = UTF8Encoding.UTF8;
+        Console.InputEncoding = UTF8Encoding.UTF8;
+        Console.CursorVisible = false;
+        Thread serverOutputThread = new Thread(UpdatePlayer);
+        serverOutputThread.Start();
+
+            lock (lockObj)
+            {
+                location.Players.Add(player);
+            }
+
+        while (true)
+        {
+            if (Console.KeyAvailable)
+            {
+                ConsoleKey key = Console.ReadKey(true).Key;
+                switch (key)
+                {
+                    case ConsoleKey.LeftArrow: player.X -= 1; break;
+                    case ConsoleKey.RightArrow: player.X += 1; break;
+                    case ConsoleKey.UpArrow: player.Y -= 1; break;
+                    case ConsoleKey.DownArrow: player.Y += 1; break;
+                }
+                byte[] buffer = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(player));
+                client.Send(buffer, serverEP);
+            }
+            location.Draw();
         }
     }
 }
